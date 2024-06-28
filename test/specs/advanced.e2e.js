828c30b027a082d0e45791528979991e20e4afbe
ekanto <umarekanto@gmail.com>
2024-06-28 10:10:46 +0600

    .

--- test/specs/advanced.e2e.js
+++ test/specs/advanced.e2e.js
@@ -2,9 +2,7 @@ describe("Advanced testing", async () => {
   it("File upload", async () => {
     await browser.url("https://the-internet.herokuapp.com/upload");
     const filePath = "./sample.jpg";
-    const remoteFilePath = await browser.uploadFile(filePath);
-    await $("#file-upload").setValue(remoteFilePath);
-    await $("#file-submit").click();
+    await browser.customFileUpload(filePath);
     await browser.pause(3000);
     const text = await $("#uploaded-files").getText();
     expect(text).toEqual("sample.jpg");
