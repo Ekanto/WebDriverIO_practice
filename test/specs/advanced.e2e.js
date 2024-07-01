c6703199cde9b05163a01e43301f1468b6aca60f
ekanto <umarekanto@gmail.com>
2024-07-02 03:02:20 +0600

    .

--- test/specs/advanced.e2e.js
+++ test/specs/advanced.e2e.js
@@ -1,4 +1,9 @@
 describe("Advanced testing", async () => {
+
+  beforeEach(async () =>{
+    await browser.url("https://the-internet.herokuapp.com/upload");
+  
+  })
   it.skip("File upload", async () => {
     await browser.url("https://the-internet.herokuapp.com/upload");
     const filePath = "./sample.jpg";
@@ -11,6 +16,13 @@ describe("Advanced testing", async () => {
   it('display title and URL', async () => {
     let result  = await browser.getTitleURL("https://the-internet.herokuapp.com/upload")
     expect(result.title).toEqual("The Internet")
+
+  })
+
+  it.only('Display browser session' ,async () =>{
+    console.log("Session before reload", await browser.sessionId)
+    await browser.reloadSession()
+    console.log("Session after reload", await browser.sessionId)
 
   })
 });
