2c1af58c13780be5bbe2de0bf34029f7de89212a
ekanto <umarekanto@gmail.com>
2024-08-12 01:36:01 +0600

    test

--- test/specs/advanced.e2e.js
+++ test/specs/advanced.e2e.js
@@ -1,9 +1,7 @@
 describe("Advanced testing", async () => {
-
-  beforeEach(async () =>{
+  beforeEach(async () => {
     await browser.url("https://the-internet.herokuapp.com/upload");
-  
-  })
+  });
   it.skip("File upload", async () => {
     await browser.url("https://the-internet.herokuapp.com/upload");
     const filePath = "./sample.jpg";
@@ -13,16 +11,28 @@ describe("Advanced testing", async () => {
     expect(text).toEqual("sample.jpg");
   });
 
-  it('display title and URL', async () => {
-    let result  = await browser.getTitleURL("https://the-internet.herokuapp.com/upload")
-    expect(result.title).toEqual("The Internet")
+  it("display title and URL", async () => {
+    let result = await browser.getTitleURL(
+      "https://the-internet.herokuapp.com/upload"
+    );
+    expect(result.title).toEqual("The Internet");
+  });
 
-  })
+  it.skip("Display browser session", async () => {
+    console.log("Session before reload", await browser.sessionId);
+    await browser.reloadSession();
+    console.log("Session after reload", await browser.sessionId);
+  });
 
-  it.only('Display browser session' ,async () =>{
-    console.log("Session before reload", await browser.sessionId)
-    await browser.reloadSession()
-    console.log("Session after reload", await browser.sessionId)
+  it.only("Create and switch new Window", async () => {
+    await browser.url("https://google.com");
+    await browser.newWindow("https://webdriver.io");
+    await browser.pause(10000);
+    await browser.switchWindow("google.com");
+    await browser.pause(3000);
+  });
 
-  })
+  it('Network throttling',  async () => {
+    
+    })
 });
